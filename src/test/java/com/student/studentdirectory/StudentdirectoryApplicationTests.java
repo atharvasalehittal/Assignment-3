92cd1f213f8b762abd90a9f5558c0550a9edbb1b
atharvasalehittal <43900387+atharvasalehittal@users.noreply.github.com>
2024-04-21 00:33:05 -0400

    Add files via upload

---
+++ src/test/java/com/student/studentdirectory/StudentdirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
