dc186ca1ce1243e8ccb933156e1f5f4850437685
atharvasalehittal <43900387+atharvasalehittal@users.noreply.github.com>
2024-04-21 00:30:52 -0400

    Delete src directory

--- src/main/java/com/student/studentdirectory/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.studentdirectory.Repository;
-
-import com.student.studentdirectory.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-    Student findBySurveyId(int surveyId);
-}
